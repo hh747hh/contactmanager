bfd678ea2fbf25d288fe90dd2d93cab465403e2a
Woo Yun <hh747hh@gmail.com>
2018-08-08 13:36:16 -0400

    added crud functionalities

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
-import Contacts from "./components/Contacts";
-import Header from "./components/Header";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
+import Header from "./components/layout/Header";
+import About from "./components/pages/About";
+import { Provider } from "./context";
 import "./App.css";
+import NotFound from "./components/pages/NotFound";
+import Test from "./test/Test";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <Contacts />
-        </div>
-      </div>
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route exact path="/test" component={Test} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
